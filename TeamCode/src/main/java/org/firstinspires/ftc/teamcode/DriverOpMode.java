fa78bfb83d6da6f4eb21623dc0349c8fd41cd9fa
claire <clairezheng.kasu@gmail.com>
2020-11-15 14:34:01 -0600

    task 2 of 11/15

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOpMode.java
@@ -3,23 +3,13 @@ package org.firstinspires.ftc.teamcode;
 import android.content.SharedPreferences;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-
-import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
-import static org.firstinspires.ftc.teamcode.AutonomousOptions.DELAY_PREF;
-import static org.firstinspires.ftc.teamcode.AutonomousOptions.DELIVER_ROUTE_PREF;
-import static org.firstinspires.ftc.teamcode.AutonomousOptions.FOUNDATION_PREF;
-import static org.firstinspires.ftc.teamcode.AutonomousOptions.PARKING_ONLY_PREF;
-import static org.firstinspires.ftc.teamcode.AutonomousOptions.PARKING_PREF;
 import static org.firstinspires.ftc.teamcode.AutonomousOptions.START_POS_MODES_PREF;
-import static org.firstinspires.ftc.teamcode.AutonomousOptions.STONE_PREF;
 
 @TeleOp(name="Newton DriverOpMode", group="Main")
 //@Disabledxxs
@@ -35,6 +25,7 @@ public class DriverOpMode extends OpMode {
     boolean fieldMode;
     boolean xAlreadyPressed = false;
     boolean yAlreadyPressed = false;
+    boolean bAlreadyPressed = false;
 
     // DriveThru combos
     RobotControl currentTask = null;
@@ -78,21 +69,27 @@ public class DriverOpMode extends OpMode {
     }
 
     private void handleVision() {
-        Pose2d currentPosition = robotVision.getNavigationLocalization();
-        telemetry.addData("Current Location", currentPosition);
-        List<Recognition> updatedRecognitions = robotVision.getRingRecognition();
-        if (updatedRecognitions != null) {
-            telemetry.addData("# Object Detected", updatedRecognitions.size());
-            // step through the list of recognitions and display boundary info.
-            int i = 0;
-            for (Recognition recognition : updatedRecognitions) {
-                telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
-                telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
-                        recognition.getLeft(), recognition.getTop());
-                telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
-                        recognition.getRight(), recognition.getBottom());
+        if(gamepad1.a){
+            Pose2d currentPosition = robotVision.getNavigationLocalization();
+            if(currentPosition!=null){
+                robotHardware.getTrackingWheelLocalizer().setPoseEstimate(currentPosition);
             }
         }
+
+//        telemetry.addData("Current Location", currentPosition);
+//        List<Recognition> updatedRecognitions = robotVision.getRingRecognition();
+//        if (updatedRecognitions != null) {
+//            telemetry.addData("# Object Detected", updatedRecognitions.size());
+//            // step through the list of recognitions and display boundary info.
+//            int i = 0;
+//            for (Recognition recognition : updatedRecognitions) {
+//                telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+//                telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+//                        recognition.getLeft(), recognition.getTop());
+//                telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+//                        recognition.getRight(), recognition.getBottom());
+//            }
+//        }
     }
 
     @Override
@@ -129,6 +126,19 @@ public class DriverOpMode extends OpMode {
             }
         }
 
+        if(gamepad1.b && !bAlreadyPressed){
+            bAlreadyPressed = true;
+            Pose2d startPose;
+            startPose = robotHardware.getTrackingWheelLocalizer().getPoseEstimate();
+            Pose2d shootingPose = new Pose2d(-5, -27, Math.toRadians(0));
+            double shootingHeading = 0;
+            Trajectory trajectory = robotHardware.getMecanumDrive().trajectoryBuilder(startPose, false).
+                    splineToSplineHeading(shootingPose, 0).build();
+            currentTask = new SplineMoveTask(robotHardware.getMecanumDrive(), trajectory);
+            currentTask.prepare();
+        } else if(!gamepad1.b && bAlreadyPressed) {
+            bAlreadyPressed = false;
+        }
         telemetry.addData("Field Mode", fieldMode);
         telemetry.addData("Gyro", gyroCurrAngle);
     }
@@ -146,6 +156,9 @@ public class DriverOpMode extends OpMode {
     }
 
     private void handleMovement() {
+        if(currentTask instanceof SplineMoveTask){
+            return;
+        }
         double turn = gamepad1.right_stick_x/2;
         double power = Math.hypot(gamepad1.left_stick_x, -gamepad1.left_stick_y);
         double moveAngle = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI/4.5;
@@ -168,6 +181,7 @@ public class DriverOpMode extends OpMode {
             turn = gamepad1.right_stick_x;
         }
 
+
         robotHardware.mecanumDriveTest(power, moveAngle, turn, 0);
     }
 
@@ -183,4 +197,6 @@ public class DriverOpMode extends OpMode {
 //        homePositionTask = new SequentialComboTask();
 //        homePositionTask.setTaskList(homePositionList);
     }
+
+
 }
